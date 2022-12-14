6671bbd03707c297ac2e34798fcea8ebb7b51422
Neson Monnerat <nmonnerat@gmail.com>
2022-12-13 21:06:59 -0300

    First commit

---
+++ anpscraper/spiders/anpspider.py
@@ -0,0 +1,30 @@
+import scrapy
+from anpscraper.items import AnpscraperItem
+
+class AnpspiderSpider(scrapy.Spider):
+    #the name of the spider
+    name = 'anpspider'
+    
+    allowed_domains = ['www.gov.br']
+    
+    #the url of the first page that we will start scraping
+    start_urls = ['https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/anuario-estatistico-2022']
+
+    def parse(self, response):
+        #here we are looping through the products and extracting the name, price & url
+        title = response.css('title::text').extract_first()
+        yield {'title': title }
+        
+        links = response.css('a::attr(href)').extract()
+        for link in links:
+            if ('.csv' in link) or ('.pdf' in link):
+                item = AnpscraperItem()
+                item['file_urls'] = [link]
+                item['original_file_name'] = link.split('/')[-1]
+                #yield {'links': link }
+                yield item
+        '''
+        file_url = response.css('.interna-link::attr(href)').get()
+        file_url = response.urljoin(file_url)
+        yield {'file_url': file_url}
+#       '''
